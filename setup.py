3f8307ad320804cb101098ad12a8cee6fb13fc0e
Gilles Legoux <gilles.legoux@gmail.com>
2016-01-12 21:59:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,17 +9,22 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name='django-settings-startup',
-    version='0.1',
+    version=read('VERSION'),
     packages=['settings-startup'],
     include_package_data=True,
     license='MIT License',
     description='A simple Django app to see settings on startup.',
-    long_description=README,
-    url='https://www.example.com/',
+    long_description=read('README.rst'),
+    url='https://github.com/glegoux/django-settings-startup/',
     author='Gilles LEGOUX',
     author_email='gilles.legoux@gmail.com',
+    tests_require=['Django>=1.8.5'],
+    test_suite='tests.test',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
