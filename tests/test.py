58e8bc542fa2f3f7c74175108ae3b751c74973d3
glegoux <gilles.legoux@gmail.com>
2016-01-19 00:20:16 +0100

    Improve test

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,8 @@
+import io
 import os
-import time
 import signal
+import sys
+import time
 
 import django
 from django.core.management import call_command
@@ -21,15 +23,50 @@ class TestCommand(TestCase):
         pass
 
     def test_with_config(self):
+        """ Test if settings are written on standard output """
+        r, w = os.pipe()
         pid = os.fork()
         if pid == 0:
-          call_command('runserver', use_config=True)
+            os.close(r)
+            with os.fdopen(w, 'w') as w:
+                sys.stdout = w
+                call_command('runserver', use_config=True)
+                sys.exit(0)
         time.sleep(2)
+        os.close(w)
         os.kill(pid, signal.SIGTERM)
+        found = False
+        with os.fdopen(r) as r:
+            msg = r.read()
+            with io.StringIO(msg) as b:
+                for line in b.readlines():
+                    line = line.rstrip()
+                    s = line.split(' = ')
+                    if len(s) == 2 and s[0] == 'APPLICATION_STAGE':
+                        found = True
+                        self.assertEqual(s[1], '\'test\'', 'APPLICATION_STAGE bad value')
+                        break
+                self.assertEqual(found, True, 'APPLICATION_STAGE does not exist')
+
 
     def test_without_config(self):
+        """ Test if settings are not written on standard output """
+        r, w = os.pipe()
         pid = os.fork()
         if pid == 0:
-          call_command('runserver', use_config=False)
+            os.close(r)
+            with os.fdopen(w, 'w') as w:
+                sys.stdout = w
+                call_command('runserver', use_config=False)
+                sys.exit(0)
         time.sleep(2)
+        os.close(w)
         os.kill(pid, signal.SIGTERM)
+        with os.fdopen(r) as r:
+            msg = r.read()
+            with io.StringIO(msg) as b:
+                for line in b.readlines():
+                    line = line.rstrip()
+                    s = line.split(' = ')
+                    if len(s) == 1 and s[0] == 'Settings django...':
+                        self.fail("Settings django should not be present on standard output")
