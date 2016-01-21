d19a6f787caf0173b1c4fef3119c5408481b91d6
Gilles Legoux <gilles.legoux@gmail.com>
2016-01-21 11:43:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
+        'Framework :: Django :: 1.8',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
